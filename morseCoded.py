f494d729103629ffbfbb2b67f8c3d76b17440a0c
BrianLim167 <blim167@gmail.com>
2017-08-08 01:48:04 -0400

    basic encoder/menu

--- morseCoded.py
+++ morseCoded.py
@@ -9,130 +9,319 @@ def rgbString(red, green, blue): # by Professor Davis
     return "#%02x%02x%02x" % (red, green, blue)
 
 from tkinter import *
+import random
 
 # Initialize the data which will be used to draw on the screen.
+class Data(object):
+    def __init__(self, width, height):
+        # load self as appropriate
+        self.runInit(width, height)
+        self.root.title("Morse Coded")
+        self.font = "Consolas 12"
+        self.initAlphabet()
+        self.menu()
+        self.interval = 200
+        self.pulse = False
+        self.pulseDelay = 10
+        self.pulseOnTime = 0
+        self.pulseOffTime = 0
+        self.pulseCode = ""
+        self.pulseEvent()
 
-def init(data):
-    # load data as appropriate
-    initAlphabet(data)
-    initWidgets(data)
+    # runInit method is modified code from Professor Davis's course website
+    def runInit(self, width, height): 
+        self.width = width
+        self.height = height
+        self.timerDelay = 100 # milliseconds
+        self.root = Tk()
+        self.canvas = Canvas(self.root, width=self.width, height=self.height)
+        self.canvas.pack()
+        self.frame = Frame(self.canvas, bg="black",
+                           width=self.width, height=self.height)
+        self.frame.pack(expand=True,fill=BOTH)
+        # set up events
+        self.root.bind("<Button-1>", lambda event:
+                                self.mousePressedWrapper(event))
+        self.root.bind("<Key>", lambda event:
+                                self.keyPressedWrapper(event))
+        self.timerFiredWrapper()
+    
+    def resetFrame(self):
+        self.pulseCode = ""
+        for w in self.frame.winfo_children():
+            w.destroy()
 
-def initAlphabet(data):
-    path = "reference/alphabet.txt"
-    text = open(path,'r', encoding="utf8").read().splitlines()
-    data.toMorse = dict()
-    data.toText = dict()
-    for line in text:
-        if (not (line == "" or line[0] == "#")):
-            char  = line.split()[0]
-            morse = line.split()[1]
-            data.toMorse[char] = morse
-            data.toMorse[char.lower()] = morse
-            data.toText[morse] = char
-    data.toMorse[" "] = "/"
-    data.toText["/"] = " "
+    def initAlphabet(self):
+        path = "reference/alphabet.txt"
+        text = open(path,'r', encoding="utf8").read().splitlines()
+        self.mapToMorse = dict()
+        self.mapToText = dict()
+        for line in text:
+            if (not (line == "" or line[0] == "#")):
+                char  = line.split()[0]
+                morse = line.split()[1]
+                self.mapToMorse[char] = morse
+                self.mapToMorse[char.lower()] = morse
+                self.mapToText[morse] = char
+        self.mapToMorse[" "] = "/"
+        self.mapToText["/"] = " "
 
-def initWidgets(data):
-    data.wText = Text(data.frame, font="Consolas 12", width=40, height=15)
-    data.wText.pack()
-    data.wText.place(x=20, y=100)
-    data.wMorse = Text(data.frame, font="Consolas 12", width=40, height=15)
-    data.wMorse.pack()
-    data.wMorse.place(x=780, y=100, anchor=NE)
+    def pulseButton(self):
+        button = Button(self.frame, bg="red",
+                              width=20, height=4,
+                              command=lambda:
+                              self.translatePulse())
+        button.pack()
+        button.place(x=400, y=500, anchor=CENTER)
+        button.bind("<ButtonPress>", lambda event:
+                    self.pulseOn())
+        button.bind("<ButtonRelease>", lambda event:
+                    self.pulseOff())
+        return button
 
-# These are the CONTROLLERs.
-# IMPORTANT: CONTROLLER does *not* draw at all!
-# It only modifies data according to the events.
-def toMorse(data, text):
-    ans = ""
-    for i in range(len(text)):
-        char = text[i]
-        if (char in data.toMorse):
-            if (i == 0): space = ""
-            else: space = " "
-            ans += space + data.toMorse[char]
-    return ans
+    def backButton(self, prevPage):
+        button = Button(self.frame, bg="red", text="X",
+                            width=6, height=3,)
+        button.pack()
+        button.place(x=790, y=10, anchor=NE)
+        button.bind("<ButtonPress>", lambda event:
+                        prevPage())
 
-def toText(data, morse):
-    ans = ""
-    morse = morse.split()
-    for i in range(len(morse)):
-        char = morse[i]
-        if (char in data.toText):
-            ans += data.toText[char]
-    return ans
+    def menu(self):
+        self.page = self.menu
+        self.resetFrame()
+        self.wTranslator = Button(self.frame, bg="red", text="Translator",
+                                  width=20,height=4)
+        self.wTranslator.pack()
+        self.wTranslator.place(x=400, y=300, anchor=CENTER)
+        self.wTranslator.bind("<ButtonPress>", lambda event:
+                              self.translator())
+        self.wEncoder = Button(self.frame, bg="red", text="Encoder",
+                               width=20,height=4)
+        self.wEncoder.pack()
+        self.wEncoder.place(x=400, y=400, anchor=CENTER)
+        self.wEncoder.bind("<ButtonPress>", lambda event:
+                           self.encoder())
+        self.wMorseSearch = Button(self.frame, bg="red", text="Morse Search",
+                                   width=20,height=4)
+        self.wMorseSearch.pack()
+        self.wMorseSearch.place(x=400, y=500, anchor=CENTER)
+        self.wMorseSearch.bind("<ButtonPress>", lambda event:
+                               self.morseSearch())
 
-def mousePressed(event, data):
-    # use event.x and event.y
-    pass
+    def translator(self):
+        self.page = self.translator
+        self.resetFrame()
+        self.wBack = self.backButton(self.menu)
+        self.wText = Text(self.frame, font=self.font, width=40, height=15)
+        self.wText.pack()
+        self.wText.place(x=20, y=100)
+        self.wMorse = Text(self.frame, font=self.font, width=40, height=15)
+        self.wMorse.pack()
+        self.wMorse.place(x=780, y=100, anchor=NE)
+        self.wButton = self.pulseButton()
 
-def keyPressed(event, data):
-    # use event.char and event.keysym
-    if (event.widget == data.wText):
-        data.wMorse.delete("1.0",END)
-        data.wMorse.insert(END, toMorse(data, data.wText.get("1.0",END)))
-    if (event.widget == data.wMorse):
-        data.wText.delete("1.0",END)
-        data.wText.insert(END, toText(data, data.wMorse.get("1.0",END)))
+    def encoder(self):
+        self.page = self.encoder
+        self.resetFrame()
+        self.wBack = self.backButton(self.menu)
+        self.wText = Text(self.frame, font=self.font, width=60, height=3)
+        self.wText.pack()
+        self.wText.place(x=400, y=100, anchor=CENTER)
+        self.setEncoderText()
+        self.wButton = self.pulseButton()
+        
+    def morseSearch(self):
+        self.page = self.morseSearch
+        self.resetFrame()
+        self.wBack = self.backButton(self.menu)
+        self.wText = Text(self.frame, font=self.font, width=27, height=15)
+        self.wText.pack()
+        self.wText.place(x=400, y=300, anchor=CENTER)
+        self.board = self.morseSearchBoard()
+        words = ["qwert","qwery","rrrrr"]
+        self.loadWords(words)
+        self.wText.insert(END, str(self.board))
 
-def timerFired(data):
-    pass
+    def morseSearchBoard(self):
+        ans = []
+        for row in range(5):
+            ans.append([])
+            for col in range(5):
+                ans[-1].append("_")
+        return ans
+
+    def loadWords(self, words):
+        for word in words:
+            attempts = 0
+            added = False
+            while (attempts < 500 and not added):
+                if self.addWord(word,
+                                random.randint(0,len(self.board)-1),
+                                random.randint(0,len(self.board[0])-1),
+                                random.randint(0,7)):
+                    added = True
+
+    def addWord(self, word, row, col, direct):
+        if (direct == 0):
+            drow = 1
+            dcol = 0
+        elif (direct == 1):
+            drow = 1
+            dcol = 1
+        elif (direct == 2):
+            drow = 0
+            dcol = 1
+        elif (direct == 3):
+            drow = -1
+            dcol = 1
+        elif (direct == 4):
+            drow = -1
+            dcol = 0
+        elif (direct == 5):
+            drow = -1
+            dcol = -1
+        elif (direct == 6):
+            drow = 0
+            dcol = -1
+        elif (direct == 7):
+            drow = 1
+            dcol = -1
+        for i in range(len(word)):
+            if (not 0 <= row + drow*i < len(self.board) or
+                not 0 <= col + dcol*i < len(self.board[0]) or
+                (self.board[row + drow*i][col + dcol*i] != "_" and
+                self.board[row + drow*i][col + dcol*i] != word[i])):
+                return False
+        for i in range(len(word)):
+            self.board[row + drow*i][col + dcol*i] = word[i]
+        return True
+
+    # These are the CONTROLLERs.
+    # IMPORTANT: CONTROLLER does *not* draw at all!
+    # It only modifies data according to the events.
+    def toMorse(self, text):
+        ans = ""
+        for i in range(len(text)):
+            char = text[i]
+            if (char in self.mapToMorse):
+                if (i == 0): space = ""
+                else: space = " "
+                ans += space + self.mapToMorse[char]
+        return ans
+
+    def toText(self, morse):
+        ans = ""
+        morse = morse.split()
+        for i in range(len(morse)):
+            char = morse[i]
+            if (char in self.mapToText):
+                ans += self.mapToText[char]
+        return ans
+
+    def pulseOn(self):
+        self.pulse = True
+
+    def pulseOff(self):
+        self.pulse = False
+
+    def pulseEvent(self):
+        if (self.pulse):
+    ##        if (self.pulseOffTime > self.interval*14):
+    ##            self.pulseCode = ""
+            if (self.pulseOffTime > self.interval*7 and self.pulseCode != ""):
+                self.pulseCode += " / "
+            elif (self.pulseOffTime > self.interval*2 and self.pulseCode != ""):
+                self.pulseCode += " "
+            if (self.pulseOffTime > 0): self.translatePulse()
+            self.pulseOffTime = 0
+            self.pulseOnTime += self.pulseDelay
+        else:
+            if (self.interval*3 < self.pulseOnTime):
+                self.pulseCode += "-"
+            elif (0 < self.pulseOnTime):
+                self.pulseCode += "."
+            if (self.pulseOnTime > 0): self.translatePulse()
+            self.pulseOnTime = 0
+            self.pulseOffTime += self.pulseDelay
+            if (self.pulseOffTime > self.interval*14):
+                self.pulseCode = ""
+        self.root.after(self.pulseDelay, self.pulseEvent)
+
+    def translatePulse(self):
+        if (self.page == self.translator):
+            self.wMorse.delete("1.0",END)
+            self.wMorse.insert(END, self.pulseCode)
+            self.wText.delete("1.0",END)
+            self.wText.insert(END, self.toText(self.pulseCode))
+        elif (self.page == self.encoder):
+            if (self.pulseCode.strip() == self.morse):
+                self.setEncoderText()
+
+    def setEncoderText(self):
+        self.morse = random.choice(list(self.mapToText.keys()))
+        self.wText.delete("1.0",END)
+        self.wText.insert(END, self.toText(self.morse))
+        
+    def mousePressed(self, event):
+        # use event.x and event.y
+        pass
+
+    def keyPressed(self, event):
+        # use event.char and event.keysym
+        if (self.page == self.translator):
+            if (event.widget == self.wText):
+                self.pulseCode = ""
+                self.wMorse.delete("1.0",END)
+                self.wMorse.insert(END, self.toMorse(self.wText.get("1.0",END)))
+            if (event.widget == self.wMorse):
+                self.pulseCode = ""
+                self.wText.delete("1.0",END)
+                self.wText.insert(END, self.toText(self.wMorse.get("1.0",END)))
+
+    def timerFired(self):
+        pass
 
 
-# This is the VIEW
-# IMPORTANT: VIEW does *not* modify data at all!
-# It only draws on the canvas.
-def redrawAll(canvas, data):
-    # draw in canvas
-    pass
+    # This is the VIEW
+    # IMPORTANT: VIEW does *not* modify data at all!
+    # It only draws on the canvas.
+    def redrawAll(self):
+        # draw in canvas
+        pass
 
+    # wrapper methods are modified code from Professor Davis's course website
+    def redrawAllWrapper(self):
+        self.canvas.delete(ALL)
+        self.redrawAll()
+        self.canvas.update()    
 
+    def mousePressedWrapper(self, event):
+        self.mousePressed(event)
+        self.redrawAllWrapper()
+
+    def keyPressedWrapper(self, event):
+        self.keyPressed(event)
+        self.redrawAllWrapper()
+
+    def timerFiredWrapper(self):
+        self.timerFired()
+        self.redrawAllWrapper()
+        # pause, then call timerFired again
+        self.canvas.after(self.timerDelay, self.timerFiredWrapper)
 
 ####################################
 ####################################
 # use the run function as-is
+# run function from course website
 ####################################
 ####################################
 
 def run(width=800, height=600):
-    def redrawAllWrapper(canvas, data):
-        canvas.delete(ALL)
-        redrawAll(canvas, data)
-        canvas.update()    
-
-    def mousePressedWrapper(event, canvas, data):
-        mousePressed(event, data)
-        redrawAllWrapper(canvas, data)
-
-    def keyPressedWrapper(event, canvas, data):
-        keyPressed(event, data)
-        redrawAllWrapper(canvas, data)
-
-    def timerFiredWrapper(canvas, data):
-        timerFired(data)
-        redrawAllWrapper(canvas, data)
-        # pause, then call timerFired again
-        canvas.after(data.timerDelay, timerFiredWrapper, canvas, data)
         
     # Set up data and call init
-    class Struct(object): pass
-    data = Struct()
-    data.width = width
-    data.height = height
-    data.timerDelay = 100 # milliseconds
-    data.root = Tk()
-    canvas = Canvas(data.root, width=data.width, height=data.height)
-    canvas.pack()
-    data.frame = Frame(canvas, bg="red", width=data.width, height=data.height)
-    data.frame.pack(expand=True,fill=BOTH)
-    init(data)
-    # set up events
-    data.root.bind("<Button-1>", lambda event:
-                            mousePressedWrapper(event, canvas, data))
-    data.root.bind("<Key>", lambda event:
-                            keyPressedWrapper(event, canvas, data))
-    timerFiredWrapper(canvas, data)
-    # and launch the app
+    data = Data(width,height)
+    # launch the app
     data.root.mainloop()  # blocks until window is closed
 
 run()
